30723632bf72157c078443688e4d3498334fefec
GabytaDev <gabyta.loyola@gmail.com>
2021-11-30 19:29:27 -0300

    ejercicios filter

--- index.js
+++ index.js
@@ -555,7 +555,7 @@ A un costo primero se le agrega la ganancia y después el IVA
 El nombre de un producto en una posición, se corresponde con el precio que está en la misma posición. 
 Por ejemplo: el producto que está en la posición 1 tiene un costo igual al elemento en la posición 1 del 
 array costos*/
-
+/*
 const productos = [ 'celular', 'notebook', 'monitor' ];
 const costos = [ 12.5, 56, 98 ];
 
@@ -580,14 +580,84 @@ const corresponder = (costos, productos) =>{
    return arrayFinal
   }
 
-console.log(corresponder(costos,productos))
+console.log(corresponder(costos,productos))*/
 
 // deberia mostrar
 // [ "celular 22.6875", "notebook 101.64", "monitor 177.87" ]
 
-// filter 
+/// EJERCICIOS FILTER///
 // a partir de un array, me quedo con ALGUNOS elementos
 // que cumplen una condicion
 // en el filter
 // hacemos un array con todos los elementos que respondan TRUE
 // a lo que estamos retornando 
+/*1
+Tenemos un array en una variable costos con números que representan costos de diferentes productos.
+Usando filter, crear un nuevo array con los precios más caros (mayores a 50) y guardarlo en la variable losMasCaros
+Mostrar el array original y el filtrado*/
+const costos = [ 39, 53, 17, 99, 7, 9, 6, 68, 54, 97, 27, 90, 92, 75, 26, 86, 22, 42, 20, 14 ];
+// codear acá la solución del ejercicio
+
+const losMasCaros = costos.filter((costo)=>{
+    return costo > 50
+})
+
+console.log(costos);
+console.log(losMasCaros);
+// deberia mostrar
+// [ 39, 53, 17, 99, 7, 9, 6, 68, 54, 97, 27, 90, 92, 75, 26, 86, 22, 42, 20, 14 ]
+// [ 53, 99, 68, 54, 97, 90, 92, 75, 86 ]*/
+
+/*2
+Tenemos un array en una variable numeros con números al azar.
+También tenemos dos arrays vacios en las dos variables numerosPares y numerosImpares.
+Utilizando filter crear un nuevo array con todos los números pares y guardalo en la variable numerosPares
+Utilizando filter crear un nuevo array con todos los números impares y guardalo en la variable numerosImpares*/
+const masNumeros = [ 43, 11, 18, 46, 44, 37, 42, 29, 9, 3, 37, 0, 40, 10, 38, 34, 25, 40, 4, 32 ];
+
+// codea acá tu solución
+const numerosPares = masNumeros.filter((numero)=>{
+    return numero % 2 == 0
+})
+const numerosImpares = masNumeros.filter((numero)=>{
+    return numero % 2 !== 0
+})
+
+console.log("pares: ", numerosPares);
+console.log("impares: ", numerosImpares);
+// deberias tener como resultado
+// pares: [18, 46, 44, 42, 0, 40, 10, 38, 34, 40, 4, 32]
+// impares: [43, 11, 37, 29, 9, 3, 37, 25]
+
+/*3
+Tenemos un array en una variable mix con varios elementos, de distintos tipos de datos.
+Usando filter, crear un nuevo array con todos los elementos que sean strings y guardalo en la variable 
+soloStrings.
+Ayuda: para saber si algo es un string en javascript, podés usar typeof ver más
+Mostrar el array resultante*/
+
+const mix = [
+  'Ut vero.',
+  2,
+  function () { console.log('hola mundo!') },
+  56,
+  'Diam rebum nonumy et.',
+  true,
+  false,
+  'Kasd stet.',
+  'Sit et dolor.',
+  null,
+  null,
+  [ 1, 2, 3],
+  'Dolore.'
+];
+
+// codear acá la solución del ejercicio
+const soloStrings = mix.filter((elemento, index)=>{
+    return typeof elemento === "string"
+})
+console.log(soloStrings);
+// deberia mostrar
+// [ 'Ut vero.', 'Diam rebum nonumy et.', 'Kasd stet.', 'Sit et dolor.', 'Dolore.' ]
+
+
